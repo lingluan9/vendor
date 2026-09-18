f8aaee5d318f1ce634309b2447bebb19377e0ffc
失凌乱 <2904354331@qq.com>
2026-09-18 12:20:01 +0800

    Change toolchain to gnu

--- loongson/set_env.sh
+++ loongson/set_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATH=$PATH:/opt/loongarch64-linux-musl-gcc13.3/bin
-export RTT_EXEC_PATH=/opt/loongarch64-linux-musl-gcc13.3/bin
-export RTT_CC_PREFIX=loongarch64-linux-musl-
+export PATH=$PATH:/opt/loongarch64-linux-gnu-gcc13.3/bin
+export RTT_EXEC_PATH=/opt/loongarch64-linux-gnu-gcc13.3/bin
+export RTT_CC_PREFIX=loongarch64-linux-gnu-
